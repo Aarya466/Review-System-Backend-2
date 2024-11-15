3b1ecd5aad03116e8d9f95c489f9486b324b80f2
Aarya466 <143737799+Aarya466@users.noreply.github.com>
2024-11-15 15:48:50 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,35 +1,30 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dbConnect from "../config/database.js";
-import router from "../Routes/FeedbackRoutes.js";
+import router from "../routes/FeedbackRoutes.js";
 
+const app = express();
 
+const PORT = 3000 || process.env.PORT;
 
-const app=express();
+app.use(express.json());
 
-const PORT=3000  || process.env.PORT;
-
-app.use(express.json())
-
+//mounting api routes
 app.use("/api/v1",router)
 
 app.get("/", (req, res)=>{
-    res.json({
-      success: true,
-      message: "Server Running Successfully"
-    })
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
   })
-  
+})
 
 dotenv.config();
-
-
 
 dbConnect();
 
 
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at port:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log("Server is running at port:", PORT);
+});
