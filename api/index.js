52b9492f78d35c4dc8261866eada95c052b458da
Aarya466 <143737799+Aarya466@users.noreply.github.com>
2024-11-15 15:46:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,6 +14,14 @@ app.use(express.json())
 
 app.use("/api/v1",router)
 
+app.get("/", (req, res)=>{
+    res.json({
+      success: true,
+      message: "Server Running Successfully"
+    })
+  })
+  
+
 dotenv.config();
 
 
